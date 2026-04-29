70ca913ce1a1d1eb91da5ee7d0a76967842ea410
horihoriho <horihoriho12345@gmail.com>
2026-04-28 23:54:27 -0700

    finished logging part

--- tcp_client.py
+++ tcp_client.py
@@ -1,5 +1,6 @@
 # -*- coding : UTF-8 -*-
 import socket
+import logging
 
 # Server connection settings
 TARGET_IP = "127.0.0.1"
@@ -7,6 +8,9 @@ TARGET_PORT = 8080
 
 # Maximum size of data received from the server at once time
 BUFFER_SIZE = 4096
+
+# Configure basic logging settings
+logging.basicConfig(filename="client.log", level=logging.INFO, format="%(levelname)s: %(message)s")
 
 
 def send_message(message):
@@ -18,19 +22,25 @@ def send_message(message):
     message : str
         The message to send to the TCP server.
     """
+    logging.info(f"Client started") 
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
         # Connect to the TCP server
         tcp_client.connect((TARGET_IP, TARGET_PORT))
+        logging.info(f"Connected to server on {TARGET_IP}:{TARGET_PORT}") 
         
         # Send the message as UTF-8 encoded bytes
         tcp_client.sendall(message.encode("utf-8"))
+        logging.info(f"Message sent: {message}") 
         
         # Receive the response from the server and decode it
         data = tcp_client.recv(BUFFER_SIZE)
         response = data.decode("utf-8")
 
         print(f"Received from server: {response}")
+        logging.info(f"Received from server: {response}") 
+
+        logging.info(f"Connection closed") 
 
 if __name__ == "__main__":
     message = input("Enter message: ")
